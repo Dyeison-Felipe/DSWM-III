e4d28b585abbadfa78e69f0c3599ceeee25119eb
Dyeison-Felipe <dyeisonfc@outlook.com>
2025-02-23 23:00:04 -0300

    fix: corrigido titulo

--- aulas/atividades_aula_2/atividade_10.php
+++ aulas/atividades_aula_2/atividade_10.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Atividade - Pangrama</title>
+  <title>Atividade - 10 - Pangrama</title>
 </head>
 <body>
   <form action="" method="POST">
